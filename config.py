40824b84fc907ac46a5725d1dac3aaabd08936f9
rodneyspark <gakururodney@gmail.com>
2020-12-17 13:59:41 +0300

    added a file

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     QUOTE_URL='http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:12Rodney@localhost/blogpost'
     #email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -20,7 +20,7 @@ class Config:
     
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:rodgaks@localhost/blogpost'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:12Rodney@localhost/blogpost'
 
 
 
